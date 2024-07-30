d4eacfc02f281b4aca407ca612547b4f899bc8a8
Krishnatejaswi S <shentharkrishnatejaswi@gmail.com>
2024-07-30 12:01:57 +0530

    Added LLVM prog 2

---
+++ LLVM_Prog/Prog2/binSrch.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int left, int right, int target) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        // Check if target is present at mid
+        if (arr[mid] == target)
+            return mid;
+
+        // If target greater, ignore left half
+        if (arr[mid] < target)
+            left = mid + 1;
+
+        // If target is smaller, ignore right half
+        else
+            right = mid - 1;
+    }
+
+    // Target is not present in array
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 23;
+    int result = binarySearch(arr, 0, n - 1, target);
+    if (result == -1)
+        printf("Element not found in array");
+    else
+        printf("Element found at index %d", result);
+    return 0;
+}
